1e6aba0c8989d945d2f01323b3ae78858cf68de3
Sui <admin@Mac-mini.local>
2020-08-04 19:37:26 -0400

    refactored for better readability

--- flabbyBird.js
+++ flabbyBird.js
@@ -1,54 +1,40 @@
-
+// draw image to canvas continuously. Set variable to get the canvas element.
 let cvs = document.getElementById("canvas");
 
+// set variable to get context of the canvas (ctx)
 let ctx = cvs.getContext("2d");
 
-// load images
+// load images, and save to unique variables
 
-let bird = new Image();
+let bird = new Image(); //our protagonist
+let bg = new Image(); //background
+let fg = new Image(); //foreground
+let pipeNorth = new Image(); //top vertical pipe
+let pipeSouth = new Image(); //bottom vertical pipe
 
-let bg = new Image();
-
-let fg = new Image();
-
-let pipeNorth = new Image();
-
-let pipeSouth = new Image();
-
-bird.src = "./images/bird.png";
-
+// image source files
+bird.src = "./images/bird.png"; 
 bg.src = "./images/bg.png";
-
 fg.src = "./images/fg.png";
-
 pipeNorth.src = "./images/pipeNorth.png";
-
 pipeSouth.src = "./images/pipeSouth.png";
 
-
+// note: drawImage() takes 5 arguments: imageName, X position, Y position, Width and Height.
 
 // some variables
-
 let gap = 85;
-
 let constant;
-
 let bX = 10;
-
 let bY = 150;
-
 let gravity = 1.5;
-
 let score = 0;
 
 // audio files
 
 let fly = new Audio();
-
 let scor = new Audio();
 
 fly.src = "./sounds/fly.mp3";
-
 scor.src = "./sounds/score.mp3";
 
 // on key down
@@ -56,11 +42,8 @@ scor.src = "./sounds/score.mp3";
 document.addEventListener("keydown",moveUp);
 
 function moveUp(){
-
     bY -= 25;
-
     fly.play();
-
 }
 
 // pipe coordinates
@@ -68,90 +51,47 @@ function moveUp(){
 let pipe = [];
 
 pipe[0] = {
-
     x : cvs.width,
-
     y : 0
-
 };
 
 // draw images
 
 function draw(){
 
-  
-
+    // giving the background a position of X = 0, and y = 0
     ctx.drawImage(bg,0,0);
 
-  
-
-  
-
     for(let i = 0; i < pipe.length; i++){
-
-      
-
         constant = pipeNorth.height+gap;
-
         ctx.drawImage(pipeNorth,pipe[i].x,pipe[i].y);
-
         ctx.drawImage(pipeSouth,pipe[i].x,pipe[i].y+constant);
-
-            
-
         pipe[i].x--;
 
-      
-
         if( pipe[i].x == 125 ){
-
             pipe.push({
-
                 x : cvs.width,
-
                 y : Math.floor(Math.random()*pipeNorth.height)-pipeNorth.height
-
             });
 
         }
 
         // detect collision
-
-      
-
         if( bX + bird.width >= pipe[i].x && bX <= pipe[i].x + pipeNorth.width && (bY <= pipe[i].y + pipeNorth.height || bY+bird.height >= pipe[i].y+constant) || bY + bird.height >=  cvs.height - fg.height){
-
             location.reload(); // reload the page
-
         }
-
-      
 
         if(pipe[i].x == 5){
-
             score++;
-
             scor.play();
-
         }
-
-      
-
-      
-
     }
 
     ctx.drawImage(fg,0,cvs.height - fg.height);
 
-  
-
     ctx.drawImage(bird,bX,bY);
 
-  
-
     bY += gravity;
-
-  
 
     ctx.fillStyle = "#000";
 
@@ -159,11 +99,7 @@ function draw(){
 
     ctx.fillText("Score : "+score,10,cvs.height-20);
 
-  
-
     requestAnimationFrame(draw);
-
-  
 
 }
 
